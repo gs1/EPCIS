6b09178a6b323e8500a0bece8d966d8379ae6392
Vladimir Alexiev <vladimir.alexiev@ontotext.com>
2021-09-23 15:10:08 +0300

    try to convert to jsonld, see #238 and README.md for details

--- Ontology/ontology-check.sh
+++ Ontology/ontology-check.sh
@@ -1,10 +1,10 @@
-perl -00ne 'print unless m{rdfs:label|\@prefix}'                EPCIS.ttl
-perl -00ne 'print unless m{rdfs:label|\@prefix}'                CBV.ttl
-perl -00ne 'print unless m{rdfs:isDefinedBy|\@prefix}'          EPCIS.ttl
-perl -00ne 'print unless m{rdfs:isDefinedBy|\@prefix}'          CBV.ttl
-perl -00ne 'print unless m{rdfs:comment|\@prefix}'              EPCIS.ttl
-perl -00ne 'print unless m{rdfs:comment|\@prefix}'              CBV.ttl
-perl -00ne 'print unless m{sw:term_status +"stable"|\@prefix}'  EPCIS.ttl
-perl -00ne 'print unless m{sw:term_status +"stable"|\@prefix}'  CBV.ttl
-perl -00ne 'print if m{TODO}'                                   EPCIS.ttl
-perl -00ne 'print if m{TODO}'                                   CBV.ttl
+perl -00ne 'print unless m{rdfs:label|\@prefix|owl:Ontology|schema:Organization|#####|"isA"}'                EPCIS.ttl
+perl -00ne 'print unless m{rdfs:label|\@prefix|owl:Ontology|schema:Organization|#####|"isA"}'                CBV.ttl
+perl -00ne 'print unless m{rdfs:isDefinedBy|\@prefix|owl:Ontology|schema:Organization|#####|"isA"}'          EPCIS.ttl
+perl -00ne 'print unless m{rdfs:isDefinedBy|\@prefix|owl:Ontology|schema:Organization|#####|"isA"}'          CBV.ttl
+perl -00ne 'print unless m{rdfs:comment|\@prefix|owl:Ontology|schema:Organization|#####|"isA"}'              EPCIS.ttl
+perl -00ne 'print unless m{rdfs:comment|\@prefix|owl:Ontology|schema:Organization|#####|"isA"}'              CBV.ttl
+perl -00ne 'print unless m{sw:term_status +"stable"|\@prefix|owl:Ontology|schema:Organization|#####|"isA"}'  EPCIS.ttl
+perl -00ne 'print unless m{sw:term_status +"stable"|\@prefix|owl:Ontology|schema:Organization|#####|"isA"}'  CBV.ttl
+perl -00ne 'print if m{TODO}' EPCIS.ttl
+perl -00ne 'print if m{TODO}' CBV.ttl
