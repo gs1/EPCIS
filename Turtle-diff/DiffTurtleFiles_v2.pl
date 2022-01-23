34bfc1f46a68983f329561752838299d7dc87a37
Vladimir Alexiev <vladimir.alexiev@ontotext.com>
2022-01-23 15:07:21 +0200

    Turtle, Turtle-simple-context, Turtle-diff: complete regen; epcis-context-simple.jsonld: https://ns.gs1.org/cbv/Comp-; DiffTurtleFiles_v2.pl: minor fixes

--- Turtle-diff/DiffTurtleFiles_v2.pl
+++ Turtle-diff/DiffTurtleFiles_v2.pl
@@ -11,49 +11,63 @@ $dirC = "./";
 open (LOG, ">$dirC"."allDiffs.txt");
 
 foreach $element (@filesA) {
-        # print "File: $element\n";
-        $diffelement=$element;
-        $diffelement=~ s/^$root$dirA/$root$dirC/;
-        #print "File: $diffelement\n";
 
-        opendir my $dir, $element or die "Cannot open directory: $!";
-        my @contents = readdir $dir;
-        closedir $dir;
+	print "File: $element\n";
 
-        my @subdirs=();
+	$diffelement=$element;
+	$diffelement=~ s/^$root$dirA/$root$dirC/;
 
-        foreach $file (@contents) {
-                unless ($file=~ /^\./) {
-                        if (-d $element.$file) {
-                                push @filesA, "$element$file"; # removed trailing forward slash
-                                mkdir "$diffelement/$file";
-                        } else {
-                                push @filepaths, "$element/$file";
-                        }
-                }
-        }
+	print "File: $diffelement\n";
+
+
+	opendir my $dir, $element or die "Cannot open directory: $!";
+	my @contents = readdir $dir;
+	closedir $dir;
+
+	my @subdirs=();
+
+
+	foreach $file (@contents) {
+		unless ($file=~ /^\./) {
+
+			if (-d $element.$file) {
+				push @filesA, $element.$file; # removed trailing forward slash
+				
+				mkdir $diffelement."/".$file;
+	
+			} else {
+				push @filepaths, $element."/".$file;
+			}
+	
+		}
+	}
+
+
 }
 
 
 foreach $pathA (@filepaths) {
-        $pathA =~ s{//}{/};
-        $pathB = $pathA;
-        $pathC = $pathA;
-        $pathB =~ s/^$root$dirA/$root$dirB/;
-        $pathC =~ s/^$root$dirA/$root$dirC/;
-        $pathC =~ s/\.ttl$/.diff/;
+	$pathB = $pathA;
+	$pathC = $pathA;
+	$pathB=~ s/^$root$dirA/$root$dirB/;
+	$pathC=~ s/^$root$dirA/$root$dirC/;
+	$pathC=~ s/\/\//\//;
+	$pathC=~ s/\.ttl$/.diff/;
 
-        print LOG "################# $pathC\n\n"; # pathA&B are printed by diff bot only if it's not empty
+	print LOG "\n$pathC\n\n";
+	
+	if ($pathA=~ /\.ttl$/) {
+	
+		$diff = `diff --ignore-space-change -U 0 $pathA $pathB`;
+	
+		open (OUT, ">$pathC");
+		print OUT $diff;
+		print LOG "$diff\n\n";
+		close (OUT);
 
-        if ($pathA=~ /\.ttl$/) {
-                $diff = `diff --ignore-space-change -U 0 $pathA $pathB`;
-                # TODO: whitelist harmless changes and use --ignore-matching-lines=...
 
-                open (OUT, ">$pathC");
-                print OUT $diff;
-                print LOG "$diff\n\n";
-                close (OUT);
-        }
+	}
+
 }
 
 close (LOG);
