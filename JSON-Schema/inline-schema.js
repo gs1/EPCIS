9e71a28128af196316368c4f730d0c132e93428e
jmcanterafonseca-iota <67736750+jmcanterafonseca-iota@users.noreply.github.com>
2022-06-08 16:48:05 +0200

    Revoke changes on the inline process

--- JSON-Schema/inline-schema.js
+++ JSON-Schema/inline-schema.js
@@ -13,9 +13,7 @@ function inline (fileName, schemaId) {
   inputJson.$id = schemaId;
 
   // We visit and inline the input Json
-  // We don't assign current definitions to the input because
-  // All definitions will be copied to the final schema
-  visit(null, null, null, inputJson, inputJson.definitions);
+  visit(null, null, null, inputJson);
 
   // Once we have the definitions we add them to the root schema
   inputJson.definitions = definitions;
@@ -26,30 +24,24 @@ function inline (fileName, schemaId) {
 // The definitions that will be filled along our visit to the nodes
 const definitions = {};
 
-function visit (parent, key, index, node, currentDefinitions) {
+function visit (parent, key, index, node) {
   if (typeof node === 'object' && !Array.isArray(node)) {
-    let currDefinitions = currentDefinitions;
     if (key === 'definitions') {
-      // The definitions are copied (only in the case of the input root schema)
+      // The definitions are copied
       Object.keys(node).forEach((aKey) => (definitions[aKey] = node[aKey]));
-      currDefinitions = node;
     }
     Object.keys(node).forEach((aKey) => {
-      visit(node, aKey, null, node[aKey], currDefinitions);
+      visit(node, aKey, null, node[aKey]);
     });
   } else if (typeof node === 'string' && key === '$ref') {
     // Here we inline the definition from the file indicated
     if (!node.startsWith('#')) {
       loadChildSchema(node);
       parent.$ref = localizeDefinition(node);
-    } else {
-        // This is a local definition that is is copied from currentDefinitions
-        const definitionName = node.split('/')[2];
-        definitions[definitionName] = currentDefinitions[definitionName];
     }
   } else if (Array.isArray(node)) {
     node.forEach((aElement, aIndex) => {
-      visit(node, key, aIndex, aElement, currentDefinitions);
+      visit(node, key, aIndex, aElement);
     });
   }
 }
@@ -59,16 +51,10 @@ function localizeDefinition (definitionPointer) {
   return `#${components[1]}`;
 }
 
-function extractDefinitionName(definitionPointer) {
-  const localizedDefinition = localizeDefinition(definitionPointer);
-  return localizedDefinition.split('/')[2];
-}
-
 function loadChildSchema (definitionPointer) {
   let fileName;
 
   fileName = definitionPointer.split('#')[0];
-  const definitionName = extractDefinitionName(definitionPointer);
 
   // Check wether the file exists, if it doesn't is a file from the child schemas
   if (!fs.existsSync(fileName)) {
@@ -76,10 +62,7 @@ function loadChildSchema (definitionPointer) {
   }
 
   const childSchema = loadJson(fileName);
-  // Definition is copied to the global object with all the definitions
-  definitions[definitionName] = childSchema.definitions[definitionName];
-  // And we only visit from definitions on
-  visit(null, null, null, childSchema.definitions, childSchema.definitions);
+  visit(null, null, null, childSchema);
 }
 
 function main () {
