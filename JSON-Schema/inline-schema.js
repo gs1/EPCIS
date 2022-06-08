3b1802057e88fb4a147a8005e21947b44e77f20f
Jose Manuel Cantera <jose.cantera@iota.org>
2022-06-08 08:43:49 +0200

    Update algorithm to generate schema

--- JSON-Schema/inline-schema.js
+++ JSON-Schema/inline-schema.js
@@ -13,7 +13,9 @@ function inline (fileName, schemaId) {
   inputJson.$id = schemaId;
 
   // We visit and inline the input Json
-  visit(null, null, null, inputJson);
+  // We don't assign current definitions to the input because
+  // All definitions will be copied to the final schema
+  visit(null, null, null, inputJson, inputJson.definitions);
 
   // Once we have the definitions we add them to the root schema
   inputJson.definitions = definitions;
@@ -24,24 +26,30 @@ function inline (fileName, schemaId) {
 // The definitions that will be filled along our visit to the nodes
 const definitions = {};
 
-function visit (parent, key, index, node) {
+function visit (parent, key, index, node, currentDefinitions) {
   if (typeof node === 'object' && !Array.isArray(node)) {
+    let currDefinitions = currentDefinitions;
     if (key === 'definitions') {
-      // The definitions are copied
+      // The definitions are copied (only in the case of the input root schema)
       Object.keys(node).forEach((aKey) => (definitions[aKey] = node[aKey]));
+      currDefinitions = node;
     }
     Object.keys(node).forEach((aKey) => {
-      visit(node, aKey, null, node[aKey]);
+      visit(node, aKey, null, node[aKey], currDefinitions);
     });
   } else if (typeof node === 'string' && key === '$ref') {
     // Here we inline the definition from the file indicated
     if (!node.startsWith('#')) {
       loadChildSchema(node);
       parent.$ref = localizeDefinition(node);
+    } else {
+        // This is a local definition that is is copied from currentDefinitions
+        const definitionName = node.split('/')[2];
+        definitions[definitionName] = currentDefinitions[definitionName];
     }
   } else if (Array.isArray(node)) {
     node.forEach((aElement, aIndex) => {
-      visit(node, key, aIndex, aElement);
+      visit(node, key, aIndex, aElement, currentDefinitions);
     });
   }
 }
@@ -51,10 +59,16 @@ function localizeDefinition (definitionPointer) {
   return `#${components[1]}`;
 }
 
+function extractDefinitionName(definitionPointer) {
+  const localizedDefinition = localizeDefinition(definitionPointer);
+  return localizedDefinition.split('/')[2];
+}
+
 function loadChildSchema (definitionPointer) {
   let fileName;
 
   fileName = definitionPointer.split('#')[0];
+  const definitionName = extractDefinitionName(definitionPointer);
 
   // Check wether the file exists, if it doesn't is a file from the child schemas
   if (!fs.existsSync(fileName)) {
@@ -62,7 +76,10 @@ function loadChildSchema (definitionPointer) {
   }
 
   const childSchema = loadJson(fileName);
-  visit(null, null, null, childSchema);
+  // Definition is copied to the global object with all the definitions
+  definitions[definitionName] = childSchema.definitions[definitionName];
+  // And we only visit from definitions on
+  visit(null, null, null, childSchema.definitions, childSchema.definitions);
 }
 
 function main () {
